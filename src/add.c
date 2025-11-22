f2a33d784c6372141c1f0e21afbe592b9743ded8
oarix <oarixgeco99@gmail.com>
2025-11-22 19:26:45 +0545

    init

---
+++ src/add.c
@@ -0,0 +1,6 @@
+#pragma once // prevent recursion
+int add(int, int); // pre decl for func
+
+int add(int a, int b) {
+  return a + b;
+}
---
+++ src/voltrite.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "add.c" // include new files like.
+
+int main() {
+  printf("ADD(20, 45) = %d\n", add(20, 45));
+  return 0;
+}
